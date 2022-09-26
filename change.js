ac1e58bd5aea2824e0c8765a740cbea8701326bb
KIM GUYMIN <111481201+K-GYUMIN@users.noreply.github.com>
2022-09-26 16:38:23 +0900

    Add files via upload

--- change.js
+++ change.js
@@ -10,6 +10,9 @@
 //     bigpic.setAttribute("src",smallpicAttribure);
 // }
 
+
+// function은 하나만 있어도 된다. 여러개 있으면 오류 걸립니다
+
 $(function(){
     $('.gnb a').click(function(){
         e.preventDefault(); //a링크 방지
@@ -31,13 +34,23 @@ $(function(){
         $('.swiper').eq($(this).index()).addClass('on');
 
     });//tab_menu li click()
-    $(function(){
+
         $('.gnb').click(function(){
             e.preventDefault(); //a링크 방지
         });
         //이벤트가를 실행시켜 주겠다 괄호안에 펑션 넣어주기, 소괄호에 e를 넣으면 방지할 수 있다
        
+        
+       
+            //영상편집 슬라이드
+            var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+            })
         }); //jquery end
-
-});//query end
-
